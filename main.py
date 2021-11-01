5323967a50eec963fe26e35fa1205ab423d021cd
Matthew Ellison-Davidson <melliso1@cs.binghamton.edu>
2021-11-01 15:33:14 -0400

    milestone 2

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-#import your controller
+import pygame 
 
 def main():
-    #Create an instance on your controller object
-    #Call your mainloop
-    
+    pygame.init()
+    team = {"lead": "Joseph", "backend": "Matthew", "frontend": "Fayeem"}
+    print("software lead is:" , team["lead"])
+    print("backend is:" , team["backend"])
+    print("frontend is" , team["frontend"])
+
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 2 LINES OF CODE ######
 main()
