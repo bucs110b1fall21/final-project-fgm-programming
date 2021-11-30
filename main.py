7036fce32005c93fb3cf1f9177ebb84c115288bc
Matthew Ellison-Davidson <melliso1@cs.binghamton.edu>
2021-11-29 22:35:20 -0500

    Milestone 4 commit

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from src import Controller
+from src import ControllerWithSubloops
 import pygame 
 
 def main():
-    pygame.init()
-    team = {"lead": "Joseph", "backend": "Matthew", "frontend": "Fayeem"}
-    print("software lead is:" , team["lead"])
-    print("backend is:" , team["backend"])
-    print("frontend is" , team["frontend"])
+    #pygame.init()
+    #team = {"lead": "Joseph", "backend": "Matthew", "frontend": "Fayeem"}
+    #print("software lead is:" , team["lead"])
+    #print("backend is:" , team["backend"])
+    #print("frontend is" , team["frontend"])
+    game = ControllerWithSubloops.Controller()
+    game.mainloop()
+
 
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 2 LINES OF CODE ######
 main()
